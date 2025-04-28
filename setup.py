6ab416798c871681d00e95161f97835b65184342
rushil-nagabhushan <rushilbhushan@gmail.com>
2025-04-28 11:43:32 -0700

    Patched setup.py for pip installation

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-import setuptools
+from setuptools import setup, find_packages
 
-if __name__ == "__main__":
-    setuptools.setup()
+setup(
+    name='nnunetv2',
+    version='2.5.1',
+    description='nnU-Net v2 - Patched for pip install',
+    author='Rushil Nagabhushan',
+    url='https://github.com/Picture-Health/nnUNet-lambda',
+    packages=find_packages(),
+    python_requires='>=3.9',
+)
