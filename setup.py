d07c364075a0ba1a33948e2b4b9a5da46863c75c
rushil-nagabhushan <rushilbhushan@gmail.com>
2025-04-28 11:58:10 -0700

    pinning requirements in setup.py to debug new import error

--- setup.py
+++ setup.py
@@ -3,9 +3,22 @@ from setuptools import setup, find_packages
 setup(
     name='nnunetv2',
     version='2.5.1',
-    description='nnU-Net v2 - Patched for pip install',
+    description='nnU-Net v2 - Patched for proper packaging',
     author='Rushil Nagabhushan',
     url='https://github.com/Picture-Health/nnUNet-lambda',
     packages=find_packages(),
     python_requires='>=3.9',
+    install_requires=[
+        "acvl-utils>=0.2,<0.3",
+        "dynamic-network-architectures>=0.3.1,<0.4",
+        "batchgenerators>=0.25",
+        "batchgeneratorsv2>=0.2",
+        "yacs",
+        "einops",
+        "dicom2nifti",
+        "imagecodecs",
+        "graphviz",
+        "tifffile",
+        "seaborn"
+    ]
 )
